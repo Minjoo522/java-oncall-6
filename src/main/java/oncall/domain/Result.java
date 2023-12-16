9751d2775499f3af761cffc7c1cbc5f3b26637ea
Minjoo <m0522inju@gmail.com>
2023-12-16 17:20:37 +0900

    refactor: Result domain의 getFinal 메서드 구조화

--- src/main/java/oncall/domain/Result.java
+++ src/main/java/oncall/domain/Result.java
@@ -2,10 +2,13 @@ package oncall.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Result {
     private final Schedule schedule;
     private final Staffs staffs;
+    private int weekdayCount;
+    private int weekendCount;
 
     public Result(Schedule schedule, Staffs staffs) {
         this.schedule = schedule;
@@ -16,49 +19,65 @@ public class Result {
         List<DayOfWeek> dayOfWeeks = List.of(DayOfWeek.values());
         List<String> results = new ArrayList<>();
         int startIndex = dayOfWeeks.indexOf(schedule.getStartDayOfWeek());
-        Staff yesterday = null;
-        int weekdayCount = 0;
-        int weekendsCount = 0;
+        Staff yesterdayStaff = null;
 
         for (int i = 0; i < getEndDateOfMonth(); i++) {
             DayOfWeek dayOfWeek = dayOfWeeks.get((startIndex + i) % dayOfWeeks.size());
-            String category = dayOfWeek.getCategory();
-            boolean isHoliday = LegalHolidays.isHoliday(schedule.getMonth(), i + 1);
-            String holidayMark = "";
-            if (isHoliday) {
-                category = "휴일";
-                holidayMark = "(휴일)";
-            }
-            Staff today = null;
-            if (category == "평일") {
-                today = staffs.getStaff(category, weekdayCount);
-                weekdayCount += 1;
-            }
-            if (category == "휴일") {
-                today = staffs.getStaff(category, weekendsCount);
-                weekendsCount += 1;
-            }
+            String category = findCategory(dayOfWeek, i + 1);
+            Staff todayStaff = findTodayStaff(category, yesterdayStaff);
 
-            if (i != 1 && yesterday == today) {
-                if (category == "평일") {
-                    today = staffs.getNextStaff(category, weekdayCount);
-                }
-                if (category == "휴일") {
-                    today = staffs.getNextStaff(category, weekendsCount);
-                }
-            }
-            yesterday = today;
-            if (isHoliday) {
-                results.add(String.format("%d월 %d일 %s %s", schedule.getMonth(), i + 1, dayOfWeek.getMessage() + "(휴일)", today.getName()));
-            }
-            if (!isHoliday) {
-                results.add(String.format("%d월 %d일 %s %s", schedule.getMonth(), i + 1, dayOfWeek.getMessage(), today.getName()));
-            }
+            yesterdayStaff = todayStaff;
+            results.add(getOneResult(dayOfWeek, todayStaff, i + 1));
         }
         return String.join(System.lineSeparator(), results);
     }
 
     private int getEndDateOfMonth() {
         return schedule.findEndDate();
+    }
+
+    private String findCategory(DayOfWeek dayOfWeek, int date) {
+        String category = dayOfWeek.getCategory();
+        boolean isHoliday = LegalHolidays.isHoliday(schedule.getMonth(), date);
+        if (isHoliday) {
+            category = "휴일";
+        }
+        return category;
+    }
+
+    private Staff findTodayStaff(String category, Staff yesterdayStaff) {
+        Staff todayStaff = null;
+        if (Objects.equals(category, "평일")) {
+            todayStaff = staffs.getStaff(category, weekdayCount);
+            weekdayCount += 1;
+        }
+        if (Objects.equals(category, "휴일")) {
+            todayStaff = staffs.getStaff(category, weekendCount);
+            weekendCount += 1;
+        }
+        if (yesterdayStaff == todayStaff) {
+            todayStaff = searchNextStaff(category);
+        }
+        return todayStaff;
+    }
+
+    private Staff searchNextStaff(String category) {
+        if (Objects.equals(category, "평일")) {
+            return staffs.getNextStaff(category, weekdayCount);
+        }
+        if (Objects.equals(category, "휴일")) {
+            return staffs.getNextStaff(category, weekendCount);
+        }
+        return null;
+    }
+
+    private String getOneResult(DayOfWeek dayOfWeek, Staff todayStaff, int date) {
+        boolean isHoliday = LegalHolidays.isHoliday(schedule.getMonth(), date);
+        if (isHoliday) {
+            return String.format(
+                    "%d월 %d일 %s %s", schedule.getMonth(), date, dayOfWeek.getMessage() + "(휴일)", todayStaff.getName()
+            );
+        }
+        return String.format("%d월 %d일 %s %s", schedule.getMonth(), date, dayOfWeek.getMessage(), todayStaff.getName());
     }
 }
